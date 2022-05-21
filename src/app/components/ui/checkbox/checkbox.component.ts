a4892fe84972b223050f8f78cf82906837103c0e
MakarovaJulia <julmakan@gmail.com>
2022-05-21 16:41:34 +0300

    checkbox working

--- src/app/components/ui/checkbox/checkbox.component.ts
+++ src/app/components/ui/checkbox/checkbox.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, forwardRef, ChangeDetectorRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+	providers: [{
+		provide: NG_VALUE_ACCESSOR,
+		useExisting: forwardRef(() => CheckboxComponent),
+		multi: true
+	}]
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent implements ControlValueAccessor {
 
 	@Input() index?: number;
-	@Input() isChecked: boolean;
-	@Input() name: string;
-	@Input() checkHandler:any;
+	@Input() name: string="";
+	value: boolean = false;
 
-
-	constructor() {
-		this.name ="";
-		this.isChecked=false;
-		this.checkHandler="";
+	constructor(private cdk: ChangeDetectorRef) {
 	}
 
-  ngOnInit(): void {
-  }
+	onChangeCallback = (v: any) => {};
+	onTouchedCallback = () => {};
 
+
+	registerOnChange(fn: any): void {
+		this.onChangeCallback = fn;
+	}
+
+	registerOnTouched(fn: any): void {
+		this.onTouchedCallback = fn;
+	}
+
+	writeValue(value: boolean): void {
+		if (value !== this.value) {
+			this.value = value;
+		}
+		this.cdk.markForCheck()
+	}
+
+	checkHandler(): void {
+		this.value = !this.value
+		this.onChangeCallback(this.value);
+		this.onTouchedCallback();
+	}
 }
